5df893acb7d45a9c6711dc48d4dc042667bdc6c7
Wagner Amaral <wamaral@bionexo.com>
2016-04-26 19:19:56 -0300

    Update dependencies versions

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.4.0"]
+                 [compojure "1.5.0"]
                  [clojail "1.0.6"]
-                 [clj-http "2.0.1"]
-                 [cheshire "5.5.0"]
+                 [clj-http "3.0.1"]
+                 [cheshire "5.6.1"]
                  [environ "1.0.2"]
                  [ring/ring-jetty-adapter "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.2.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler clj-slackbot.core.handler/app}
   :uberjar-name "clj-slackbot.jar"
