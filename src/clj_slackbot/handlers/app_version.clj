592353e91c83e0cda3c88fdbee686594a032c5a9
Wagner Amaral <wamaral@bionexo.com>
2016-05-02 19:58:19 -0300

    Add !version command (bionexo apps)

---
+++ src/clj_slackbot/handlers/app_version.clj
@@ -0,0 +1,36 @@
+(ns clj-slackbot.handlers.app-version
+  (:require [clj-slackbot.channels :refer [get-pub]]
+            [clj-http.client :as client]
+            [clojure.core.async :refer [<! chan go-loop sub]]
+            [clojure.string :as s]))
+
+(def version-chan (chan))
+(sub (get-pub :message-pub) :version version-chan)
+
+(def url-regex #"(?im)^<?(https?:\/\/)?([^\/:]+)(:\d+)?\/?")
+
+(def shortcut
+  {:bioidprod "https://login.bio/version"
+   :bioidbeta "https://login.stage.cloud.bionexo.com.br/version"
+   :regeneprod "https://platform.bio/version"
+   :regenebeta "https://platform.stage.cloud.bionexo.com.br/version"})
+
+(defn parse-url [url]
+  (let [sanitized (s/replace url #"[<>]" "")
+        [_ protocol domain port] (re-matches url-regex sanitized)
+        proto (if (s/blank? protocol) "https://" protocol)]
+    (get shortcut (keyword sanitized)
+         (str proto domain port "/version"))))
+
+(go-loop []
+  (let [message (<! version-chan)
+        body (:body message)
+        url (parse-url body)
+        writer (:out message)]
+    (if (= body "shortcuts")
+      (writer (println-str shortcut))
+      (try
+        (writer (str url " " (:body (client/get url))))
+        (catch Exception e
+          (writer (str "Error: no version information found. Is the URL well-formed? " url)))))
+    (recur)))
--- src/clj_slackbot/load_handlers.clj
+++ src/clj_slackbot/load_handlers.clj
@@ -1,2 +1,3 @@
 (ns clj-slackbot.load-handlers
-  (:require [clj-slackbot.handlers.ping]))
+  (:require [clj-slackbot.handlers.ping]
+            [clj-slackbot.handlers.app-version]))
